80912364140bc98c01e8b4090dfde7922b073106
Anton Gusev <FrostTG@gmail.com>
2021-11-24 17:36:09 +0300

    Добавьте файлы проекта.

---
+++ Home14WpfApp/CategoryConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Home14WpfApp
+{
+    class CategoryConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((int)value==0)
+            {
+                return "Data/food.jpg";
+            }
+            return "Data/appliances.jpg";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
